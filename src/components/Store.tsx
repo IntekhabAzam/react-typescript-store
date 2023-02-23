eaf4dcf3565213ebb2ee4509cad14a7a4772ab74
INTEKHAB AZAM <iazam0910@gmail.com>
2023-02-23 22:23:19 +0530

    minor styling update

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -4,9 +4,8 @@ import { StoreItem } from './StoreItem';
 
 export function Store () {
     return (
-        <div className='px-2'>
-        <h1 className='mb-3'>Store</h1>
-        <Row xs={1} md={2} lg={3} className="g-3">
+        <div className='px-2 pt-2'>
+        <Row xs={1} sm={2} md={3} lg={4} className="g-3">
             {storeItems.map(item => (
                 <Col key={item.id}><StoreItem {...item}/></Col>
             ))}
